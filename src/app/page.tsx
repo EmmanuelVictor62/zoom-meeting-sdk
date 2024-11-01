ca4f0164c40efefb427a468cd1491957649efab9
royalManuel <104584134+EmmanuelVictor62@users.noreply.github.com>
2024-11-01 19:56:53 +0100

    feat: Instantiated zoom-meeting-sdk functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,71 @@
-import Image from "next/image";
+"use client";
+import React from "react";
 import styles from "./page.module.css";
+import ZoomMtgEmbedded from "@zoom/meetingsdk/embedded";
 
-export default function Home() {
+const Home: React.FC = () => {
+  const client = ZoomMtgEmbedded.createClient();
+
+  const authEndpoint = "http://localhost:3000";
+  const sdkKey = "";
+  const meetingNumber = "";
+  const passWord = "";
+  const role = 0;
+  const userName = "React";
+  const userEmail = "";
+  const registrantToken = "";
+  const zakToken = "";
+
+  const getSignature = async () => {
+    try {
+      const req = await fetch(authEndpoint, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          meetingNumber: meetingNumber,
+          role: role,
+        }),
+      });
+      const res = await req.json();
+      const signature = res.signature as string;
+      startMeeting(signature);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  async function startMeeting(signature: string) {
+    const meetingSDKElement = document.getElementById("meetingSDKElement")!;
+    try {
+      await client.init({
+        zoomAppRoot: meetingSDKElement,
+        language: "en-US",
+        patchJsMedia: true,
+        leaveOnPageUnload: true,
+      });
+      await client.join({
+        signature: signature,
+        sdkKey: sdkKey,
+        meetingNumber: meetingNumber,
+        password: passWord,
+        userName: userName,
+        userEmail: userEmail,
+        tk: registrantToken,
+        zak: zakToken,
+      });
+      console.log("joined successfully");
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    <div className={styles["home"]}>
+      <h1>Zoom Meeting SDK Sample React</h1>
+      <div id="meetingSDKElement"></div>
+      <button onClick={getSignature}>Join Meeting</button>
     </div>
   );
-}
+};
+
+export default Home;
